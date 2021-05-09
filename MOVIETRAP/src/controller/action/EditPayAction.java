175cd23dd150a5ff4ec868c5a10ab5ec2e172c33
kplbb <katpark16@gmail.com>
2021-05-09 19:23:02 +0900

    add: editpayaction

--- MOVIETRAP/src/controller/action/EditPayAction.java
+++ MOVIETRAP/src/controller/action/EditPayAction.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class EditPayAction implements Action {
-
+    
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
