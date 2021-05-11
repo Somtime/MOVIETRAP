9e0f33a699f7b1bcd534f76a1acb7bee2551c9d3
jhlee224 <jhleee224@gmail.com>
2021-05-11 18:30:49 +0900

    insert data

--- MOVIETRAPDB.sql
+++ MOVIETRAPDB.sql
@@ -30,7 +30,8 @@ pay_name    VARCHAR2(100),
 pay_num     NUMBER,
 pay_num_lstfour NUMBER,
 pay_cvc     NUMBER  ,
-pay_date    DATE    DEFAULT SYSDATE
+pay_date    DATE    DEFAULT SYSDATE,
+admin       CHAR    DEFAULT 'n'
 );
 
 -- Review Table
@@ -49,25 +50,28 @@ edit_date   DATE    DEFAULT SYSDATE
 DROP SEQUENCE review_seq;
 CREATE SEQUENCE review_seq START WITH 1; 
 
--- Worker Table
-DROP TABLE worker CASCADE CONSTRAINTS;
-CREATE TABLE worker (
-id  VARCHAR2(50)    PRIMARY KEY,
-pwd     VARCHAR2(50)    NOT NULL,
-name    VARCHAR2(50)    NOT NULL,
-image   VARCHAR2(50)    DEFAULT 'worker.png'
-);
-
 -- Qna Table
 DROP TABLE qna CASCADE CONSTRAINTS;
 CREATE TABLE qna (
 cseq        NUMBER  PRIMARY KEY,
-member_id   VARCHAR2(50)   REFERENCES member(email),
-worker_id   VARCHAR2(50)    REFERENCES worker(id),
+send_id   VARCHAR2(50)   REFERENCES member(email),
+rcvd_id   VARCHAR2(50)    REFERENCES member(email),
 chat_content VARCHAR2(50)   NOT NULL,
 chat_time    TIMESTAMP      DEFAULT SYSDATE
 );
 
 --QNA sequence
-DROP SEQUENCE review_seq;
-CREATE SEQUENCE review_seq START WITH 1; 
+DROP SEQUENCE qna_seq;
+CREATE SEQUENCE qna_seq START WITH 1; 
+
+---- 데이터 삽입 
+
+-- member 
+INSERT INTO member (email, pwd, name, phone, pay_name, pay_num, pay_num_lstfour, pay_cvc) values
+('kdpark@email.com','1111','박길동','010-1111-1111','payname','111111111111','1111','111');
+INSERT INTO member (email, pwd, name, phone, admin) values
+('kdlee@email.com','2222','이길동','010-1111-1111','y');
+
+-- qna
+INSERT INTO qna(cseq, send_id, rcvd_id, chat_content) values(qna_seq.nextval, 'kdpark@email.com', 'kdlee@email.com','질문');
+INSERT INTO qna(cseq, send_id, rcvd_id, chat_content) values(qna_seq.nextval, 'kdlee@email.com', 'kdpark@email.com','답변');
