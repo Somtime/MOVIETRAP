55aaeb94be5134f6c75edbe9f4b69fafe2758cf1
Yu geonhee <rghee0821@gmail.com>
2021-05-22 21:54:58 +0900

    mainaction

--- MOVIETRAP/src/controller/action/mainAction.java
+++ MOVIETRAP/src/controller/action/mainAction.java
@@ -75,7 +75,7 @@ public class mainAction implements Action {
 		String movieid = obj.get("id").toString();
 		//	trailer_id put into result
 		result.put("trailer_id", movieid);
-		System.out.println(obj);
+		//System.out.println(obj);
 		// 끝 : movieid
 		
 		
@@ -86,19 +86,20 @@ public class mainAction implements Action {
 		videoData = getData(videoURL);
 		JSONObject video = (JSONObject) videoData.get(0);
 		String trailerkey = video.get("key").toString();
-		System.out.println(video);
+		//System.out.println(video);
 
 		// 끝 : trailerkey
 		
-		//	get trailer genres
-//		String movieDetailURL ="https://api.themoviedb.org/3/movie/"+ movieid +"?api_key=e520d648beeee23f00a8b3386b9dec08&language=en-US";
-//		JSONObject movieDetailData = getData(movieDetailURL);
-//		System.out.println(movieDetailData);
-//		JSONArray genres = (JSONArray) movieDetailData.get("genres");
-//		JSONObject trailer_genres = new JSONObject();
-//		for(int p=0; p<genres.size(); p++) {
-//		 trailer_genres = (JSONObject) genres.get(p);
-//		}
+		//get trailer genres
+		System.out.println("movieid : " + movieid);
+		String movieDetailURL ="https://api.themoviedb.org/3/movie/"+ movieid +"?api_key=e520d648beeee23f00a8b3386b9dec08&language=en-US";
+		JSONObject movieDetailData = getData(movieDetailURL);
+		System.out.println(movieDetailData);
+		JSONArray genres = (JSONArray) movieDetailData.get("genres");
+		JSONObject trailer_genres = new JSONObject();
+		for(int p=0; p<genres.size(); p++) {
+		 trailer_genres = (JSONObject) genres.get(p);
+		}
 		
 		//	push to result
 //		result.put("genres", trailer_genres);
@@ -106,7 +107,7 @@ public class mainAction implements Action {
 		
 		// popmoviekey를 key로 result에 넣음
 		result.put("popmoviekey", trailerkey);
-		System.out.println(result);
+		//System.out.println(result);
 	
 		
 		PrintWriter out = response.getWriter();
@@ -146,10 +147,12 @@ public class mainAction implements Action {
 		JSONObject jsonObject = new JSONObject();
 		try {
 			jsonObject = (JSONObject) jsonParser.parse(responseBody);
+			//System.out.println("jsonObject : " + jsonObject);
 		} catch (ParseException e) {
 			e.printStackTrace();
 			}
 		JSONArray jsonArray = (JSONArray)jsonObject.get("results");
+		//System.out.println("jsonArray : " + jsonArray);
 		JSONObject data = new JSONObject();
 		for (int i = 0; i < jsonArray.size(); i ++) {
 			data.put(i, jsonArray.get(i));
